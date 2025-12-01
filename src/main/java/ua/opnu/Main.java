ddc9bdd352ac43046431a53a9013856a42eaa36d
vladaguniya <9100299@stud.op.edu.ua>
2025-12-02 01:01:11 +0200

    Update Main.java

--- src/main/java/ua/opnu/Main.java
+++ src/main/java/ua/opnu/Main.java
@@ -2,6 +2,18 @@ package ua.opnu;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person[] people = {
+                new Student("Петренко", "Іван", 20, "IT-21", "KB12345678"),
+                new Lecturer("Іванов", "Петро", 45, "Комп'ютерних наук", 25000.0),
+                new Person("Сидоренко", "Марія", 30),
+                new Student("Коваленко", "Анна", 19, "ЕК-12", "KB87654321"),
+                new Lecturer("Захаренко", "Ольга", 52, "Прикладної математики", 32000.0)
+        };
+
+        System.out.println("--- Список людей в масиві ---");
+
+        for (Person person : people) {
+            System.out.println(person.toString());
+        }
     }
 }
